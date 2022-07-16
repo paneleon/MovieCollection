d7b7a1162c155c900b4035df74622cfd4ba37c08
Eleonora Panussyants <paneleon06@gmail.com>
2022-07-15 23:14:59 -0400

    Snack activity

--- app/src/main/java/com/example/moviecollection/views/SnacksActivity.java
+++ app/src/main/java/com/example/moviecollection/views/SnacksActivity.java
@@ -2,7 +2,11 @@ package com.example.moviecollection.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.example.moviecollection.R;
 
@@ -12,5 +16,27 @@ public class SnacksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snacks);
+
+        TextView linkView = findViewById(R.id.popcorn_recipe);
+        linkView.setMovementMethod(LinkMovementMethod.getInstance());
+
+//        handleIntent(getIntent());
     }
+
+//    protected void onNewIntent(Intent intent) {
+//        super.onNewIntent(intent);
+//        handleIntent(intent);
+//    }
+//
+//    private void handleIntent(Intent intent) {
+//        String appLinkAction = intent.getAction();
+//        Uri appLinkData = intent.getData();
+//
+//        if (Intent.ACTION_VIEW.equals(appLinkAction) && appLinkData != null){
+//            String recipeId = appLinkData.getLastPathSegment();
+//            Uri appData = Uri.parse("content://com.recipe_app/recipe/").buildUpon()
+//                    .appendPath(recipeId).build();
+//            showRecipe(appData);
+//        }
+//    }
 }
