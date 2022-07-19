ce7257348221e3254221fff259129aa7c653b2a1
Eleonora Panussyants <paneleon06@gmail.com>
2022-07-18 23:08:01 -0400

    Get popular movies functionality

--- app/src/main/java/com/example/moviecollection/views/RecommendationsActivity.java
+++ app/src/main/java/com/example/moviecollection/views/RecommendationsActivity.java
@@ -56,7 +56,7 @@ public class RecommendationsActivity extends AppCompatActivity {
         Button buttonForLatest = findViewById(R.id.button_latest_movies);
         Button buttonForTop = findViewById(R.id.button_top_movies);
 
-        buttonForLatest.setOnClickListener(v -> getLatestMovies());
+        buttonForLatest.setOnClickListener(v -> getPopularMovies());
 
         buttonForTop.setOnClickListener(v -> getTopMovies());
 
@@ -64,30 +64,9 @@ public class RecommendationsActivity extends AppCompatActivity {
 
     }
 
-    private void getLatestMovies() {
-        String endpoint = String.format("https://api.themoviedb.org/3/movie/popular?api_key=%s&language=en-US&page=1", API_KEY);
-//
-//        ArrayList<String> result =  new ArrayList<>(0);
-//
-//        URL url = new URL(endpoint);
-//        URLLConnection connection = url.openConnection();
-//
-//
-//        Thread t = new Thread(new Runnable() {
-//            public void run() {
-//                // Perform Network operations and processing.
-//                final MyDataClass result = loadInBackground();
-//                // Synchronize with the UI thread to post changes.
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        System.out.println(result);
-//                    }
-//                });
-//            }
-//        });
-//        t.start();
-
+    private void getPopularMovies() {
+        movieList.clear();
+        String endpoint = String.format("https://api.themoviedb.org/3/movie/popular?api_key=%s&language=en-US", API_KEY);
 
         RequestQueue requestQueue;
         requestQueue = Volley.newRequestQueue(this);
@@ -110,15 +89,18 @@ public class RecommendationsActivity extends AppCompatActivity {
                             Log.d("my-api", "==== " + jsonObject.getString("id"));
                             movieList.add(new Movie(Integer.parseInt(jsonObject.getString("id")),
                                             jsonObject.getString("title"),
-                                            jsonObject.getString("overview")
+                                            jsonObject.getString("overview"),
+                                            (jsonObject.has("release_date") ? jsonObject.getString("release_date") : ""),
+                                            Double.parseDouble(jsonObject.getString("vote_average"))
                                     )
                             );
                         }
+                        moviesRecyclerView.setAdapter(new MovieListAdapter(movieList, MovieListAdapter.ListType.RECOMMENDATIONS, movieViewModel));
                     }
                 } catch (Exception e){
                     e.printStackTrace();
                 }
-                moviesRecyclerView.setAdapter(new MovieListAdapter(movieList, MovieListAdapter.ListType.RECOMMENDATIONS, movieViewModel));
+
             }
         }, new Response.ErrorListener(){
             @Override
@@ -132,8 +114,53 @@ public class RecommendationsActivity extends AppCompatActivity {
 
 
     private void getTopMovies(){
+        movieList.clear();
+        String endpoint = String.format("https://api.themoviedb.org/3/movie/top_rated?api_key=%s&language=en-US", API_KEY);
 
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, endpoint,null, new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results");
+
+                    if (results.length() == 0){
+                        emptyResultImage.setVisibility(View.VISIBLE);
+                    } else {
+                        emptyResultImage.setVisibility(View.INVISIBLE);
+                        for (int i = 0; i < results.length(); i++) {
+                            JSONObject jsonObject = results.getJSONObject(i);
+
+                            Log.d("my-api", "==== " + jsonObject.getString("title"));
+                            Log.d("my-api", "==== " + jsonObject.getString("overview"));
+                            Log.d("my-api", "==== " + jsonObject.getString("id"));
+                            movieList.add(new Movie(Integer.parseInt(jsonObject.getString("id")),
+                                            jsonObject.getString("title"),
+                                            jsonObject.getString("overview"),
+                                            (jsonObject.has("release_date") ? jsonObject.getString("release_date") : ""),
+                                            Double.parseDouble(jsonObject.getString("vote_average"))
+                                    )
+                            );
+                        }
+                        moviesRecyclerView.setAdapter(new MovieListAdapter(movieList, MovieListAdapter.ListType.RECOMMENDATIONS, movieViewModel));
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("my-api",error.getMessage());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
+
+//    public ArrayList<Movie> getMoviesFromAPI(){
+//
+//    }
 }
 
 
